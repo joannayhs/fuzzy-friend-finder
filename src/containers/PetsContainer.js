70c9b5fa50c69c1e0d08e29a765503fea039c1d0
joannayhs <joannayuhas@gmail.com>
2020-02-25 20:19:06 -0500

    created container components for each route

---
+++ src/containers/PetsContainer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+export default class PetsContainer extends Component {
+
+    render(){
+        return (
+            <div>
+                <h1>Pets Container</h1>
+            </div>
+        )
+    }
+
+}
