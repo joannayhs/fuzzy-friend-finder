080179cc9a763e3d3fcca12ac3a879fdc342c76f
joannayhs <joannayuhas@gmail.com>
2020-02-25 20:11:36 -0500

    added routes to app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import PetsContainer from './containers/PetsContainer'
+import Application from './containers/ApplicationContainer'
+import Profile from './containers/ProfileContainer'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route exact path='/' component={PetsContainer}/>
+        <Route exact path='/application' component={Application}/>
+        <Route exact path='/profile' component={Profile}/>
+      </div>
+    </Router>
+    
   );
 }
 
