8840c4151d08eb68cce266b915d06b8fa581e2b7
joannayhs <joannayuhas@gmail.com>
2020-02-29 13:08:52 -0500

    photos load on DOM

--- front-end/src/components/Pets/Pet.js
+++ front-end/src/components/Pets/Pet.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 function Pet(props){
-    console.log(props.pet)
+    function loadPhotos(){
+        if(props.pet.photos.length > 0){
+            return <img src={props.pet.photos[0].small}/>
+        }
+    }
 
     return (
-        <div id={props.pet.id}>
-            <img src={props.pet.photos[0]}/>
-            {props.pet.name}
+        <div id={props.pet.id} className='pet'>
+            <h2>{props.pet.name}</h2><br/>
+            {loadPhotos()}
+            {props.pet.type}<br/>
+            {props.pet.species}<br/>
         </div>
     )
 }
