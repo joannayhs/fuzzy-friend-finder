270d64d062cb046b89627f901401ee45ae45848f
joannayhs <joannayuhas@gmail.com>
2020-03-06 14:19:13 -0500

    added application resource to backend

---
+++ back-end/db/migrate/20200306191804_create_applications.rb
@@ -0,0 +1,13 @@
+class CreateApplications < ActiveRecord::Migration[6.0]
+  def change
+    create_table :applications do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
