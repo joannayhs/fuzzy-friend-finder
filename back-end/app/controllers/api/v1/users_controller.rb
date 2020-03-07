5edc63e55654449bb911a6b6ccd70fea85556068
joannayhs <joannayuhas@gmail.com>
2020-03-07 10:28:23 -0500

    added index and create actions to user controller

--- back-end/app/controllers/api/v1/users_controller.rb
+++ back-end/app/controllers/api/v1/users_controller.rb
@@ -2,8 +2,24 @@ class Api::V1::UsersController < ApplicationController
 
     def index 
         users = User.all
-        render json: users
+        render json: users, except: [:password_digest]
     end 
 
-    
+    def create 
+        user = User.new(user_params)
+        if user.save
+            session[:user_id]  = user.id 
+            render json: user, except: [:password_digest]
+        else 
+            render json: {errors: user.errors.full_messages}
+        end 
+    end 
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end 
+
+
 end
