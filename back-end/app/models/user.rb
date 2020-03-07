085d1d628ec99c3a6d3d75031fa655668ec774c8
joannayhs <joannayuhas@gmail.com>
2020-03-07 10:16:14 -0500

    added validations to user model

--- back-end/app/models/user.rb
+++ back-end/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :applications
+    has_secure_password
+
+    EMAIL_REGEX = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
+    validates :email, presence: true, uniquenes: true, format: EMAIL_REGEX
+    validates :password, presence: true, length: {in: 6..20}
+
 end
